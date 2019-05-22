b94419871310303eff2f01fc5986011cc470259e
Christian Martínez <christt105@gmail.com>
2019-05-22 21:01:44 +0200

    mi carro me lo robaron

---
+++ Project/Assets/Car.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    private Vector3 initial_pos;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initial_pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("car trigger"))
+        {
+            transform.position = initial_pos;
+        }
+    }
+}
