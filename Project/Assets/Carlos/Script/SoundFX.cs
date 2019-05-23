3cf4efc75cbd6ce27f0a90c436934dff79d48a90
CarlosUPC <wolfsea.yt@gmail.com>
2019-05-23 21:38:16 +0200

    add sound script

---
+++ Project/Assets/Carlos/Script/SoundFX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFX : MonoBehaviour
+{
+
+    public AudioClip Sonido;
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = Sonido;
+        GetComponent<AudioSource>().enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player") GetComponent<AudioSource>().Play();
+
+    }
+}
+
