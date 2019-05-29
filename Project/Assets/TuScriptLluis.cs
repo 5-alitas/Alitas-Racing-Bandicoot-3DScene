03f6a2418e88b41a42a129bb2edc34663896eb65
Christian Martínez <christt105@gmail.com>
2019-05-29 19:18:50 +0200

    El script del tronco del lluis

---
+++ Project/Assets/TuScriptLluis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuScriptLluis : MonoBehaviour
+{
+    private Vector3 v_rot;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        v_rot = new Vector3(0, 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(v_rot, speed);
+    }
+}
