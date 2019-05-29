e0bd8ffa7bb15790f52c169f96272a7b23208195
youis11 <lluis.moreu@gmail.com>
2019-05-30 00:55:52 +0200

    added 2 assets lowpoly

--- Project/Assets/TuScriptLluis.cs
+++ Project/Assets/TuScriptLluis.cs
@@ -9,7 +9,7 @@ public class TuScriptLluis : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        v_rot = new Vector3(0, 0, 1);
+        v_rot = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
